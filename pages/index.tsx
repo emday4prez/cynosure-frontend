774803b897df5dce2d10325d7138a894c9daa93a
Emerson <emerson.day@gmail.com>
2022-12-27 15:12:59 -0800

    added nav and footer and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-
-      </main>
+      <div>hello to the world</div>
     </>
   )
 }
