dd9d987c5a0255c9f2ebd828338992d378089162
Emerson <emerson.day@gmail.com>
2022-12-27 22:15:52 -0800

    changed nav to daisy ui

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,10 @@
 function Hero() {
   return (
-      <div className='flex flex-col justify-center items-center min-h-screen'>
-        <h1 className='text-5xl'>welcome to cynosure</h1>
-        <h2>a website to keep track of your intermittent fasting</h2>
-      </div>
-  )
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-5xl">welcome to cyno</h1>
+      <h2>a website to keep track of your intermittent fasting</h2>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
