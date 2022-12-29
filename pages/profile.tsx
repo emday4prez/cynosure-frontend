76ed21c35db5e93e9733b89393cc86c961e72273
Emerson <emerson.day@gmail.com>
2022-12-28 17:36:42 -0800

    added file input for profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,15 @@
+import { fetcher } from '../utils/api';
+import { getTokenFromServerCookie } from '../utils/auth';
 import { useFetchUser } from '../utils/authContext';
 
-const Profile = () => {
+const Profile = ({ avatar }) => {
   const { user, loading } = useFetchUser();
+  const uploadToClient = (e) => {};
+
+  const uploadToServer = async () => {};
 
   return (
-    <div className="flex flex-col items-center  min-h-screen">
+    <div className="flex flex-col items-center min-h-screen">
       <h1 className="text-5xl font-bold">
         {' '}
         welcome back{' '}
@@ -12,8 +17,49 @@ const Profile = () => {
           {user}
         </span>
       </h1>
+      {avatar === 'default_avatar' && (
+        <div>
+          <h4>select an image to upload</h4>
+          <input type="file" onChange={uploadToClient} />
+          <button
+            className="md:p-2 rounded py-2 text-black bg-blue-400 p-2"
+            type="submit"
+            onClick={uploadToServer}
+          >
+            Set Profile Image
+          </button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Profile;
+
+export async function getServerSideProps({ req }) {
+  const jwt = getTokenFromServerCookie(req);
+
+  if (!jwt) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  } else {
+    const responseData = await fetcher(
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/users/me`,
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    const avatar = responseData.avatar ? responseData.avatar : 'default_avatar';
+    return {
+      props: {
+        avatar,
+      },
+    };
+  }
+}
